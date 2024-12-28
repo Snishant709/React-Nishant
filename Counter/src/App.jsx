3612afbb2bb0d6aa15d3837007ee434b40e2ccbb
Snishant709 <117982133+Snishant709@users.noreply.github.com>
2024-12-28 11:44:23 +0000

    counter

---
+++ Counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  // useState is a React Hook that allows us to manage state in a functional component.
+// 'counter' is the current state value, initially set to 8.
+// 'setCounter' is the function to update the 'counter' state.
+// - The first value (counter) is the current state.
+// - The second value (setCounter) is the function to update the state you can change the function name as per choice.
+// - Initial state here is set to 8.
+const [counter, setCounter] = useState(8);
+
+const addValue=()=>
+{
+  if(counter<20)
+  setCounter(counter+1)
+}
+const removeValue=()=>
+{
+  if(counter>0)
+setCounter(counter-1)
+
+}
+  return (
+    <>
+        <h1>Counter made by Nishant</h1>
+        <h3>Counter {counter}</h3>
+        <button onClick={addValue}>Add value {counter}</button>
+        <br/><br/>
+        <button onClick={removeValue}>Remove Value {counter}</button>
+    </>
+  )
+  
+}
+
+export default App
