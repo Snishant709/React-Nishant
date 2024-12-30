53c826adf5f8b950276a7bc22b27cd4addb90952
Snishant709 <117982133+Snishant709@users.noreply.github.com>
2024-12-30 09:59:34 +0000

    props and tailwind

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+//always ensure that img or any other tags must be closed properly i.e </>
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 font-light rounded-xl border-4 border-yellow-400 shadow hover:border-transparent hover:bg-white mb-4'>This is Nishant React Project</h1>
+      
+    </>
+  )
+}
+
+export default App
