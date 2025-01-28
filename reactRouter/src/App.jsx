b1bf470301fa3f726547974a9eeed9caa180c05f
Snishant709 <117982133+Snishant709@users.noreply.github.com>
2025-01-28 13:28:49 +0000

    changes

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-600'>My name is Nishant</h1>
+      
     </>
   )
 }
